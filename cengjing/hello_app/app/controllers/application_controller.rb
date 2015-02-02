a8dda78be795e8906f40873b58d1a7bf1c96f8f9
cengjing <hnbianch@gmail.com>
2015-02-02 18:15:09 +0800

    Update routes.rb

--- cengjing/hello_app/app/controllers/application_controller.rb
+++ cengjing/hello_app/app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     def hello
-       render text: "hello, world!"
-    end 
+      render text: "&iexclhola, mundo!"
+    end
+    def goodbye
+      render text: "goodbye,world!"
+    end
 end
