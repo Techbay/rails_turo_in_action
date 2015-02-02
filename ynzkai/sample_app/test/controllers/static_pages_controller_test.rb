bdc24a82f36bb013c47120828ad8a5e48d2412e3
ynzkai <ynzkai@gmail.com>
2015-02-02 07:32:24 -0800

    fix a mistake

--- ynzkai/sample_app/test/controllers/static_pages_controller_test.rb
+++ ynzkai/sample_app/test/controllers/static_pages_controller_test.rb
@@ -24,10 +24,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
-    get :about
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
-
 end
