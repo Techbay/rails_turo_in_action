4c7141bfc71e51733bf8184ecf6cb467427d8ace
killernova <enternicky@gmail.com>
2015-02-03 12:06:30 +0800

    add

---
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title" , "Help | #{@base_title}"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title" , "About | #{@base_title}"
+  end
+  
+  test "should get contract" do
+    get :contract
+    assert_response :success
+    assert_select "title", "Contract | #{@base_title}"
+  end
+
+end
