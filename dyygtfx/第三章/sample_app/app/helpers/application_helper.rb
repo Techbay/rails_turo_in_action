60d73dcb395e984a940ab04004c179d23148ba8c
Go7hic <gtfx0209@qq.com>
2015-02-03 22:34:25 +0800

    更新第四章

--- dyygtfx/第三章/sample_app/app/helpers/application_helper.rb
+++ dyygtfx/第三章/sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '') #定义方法，参数可选
+    base_title = "Ruby on Rails Tutorial Sample App" # 变量赋值
+    if page_title.empty? # 布尔测试
+      base_title #隐式返回值
+    else
+      "#{page_title} | #{base_title}" # 字符串插值
+    end
+  end
 end
