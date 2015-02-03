fb0369c933c61af1e6fff2251ef66fd9d471312e
AssIstne <463636701@qq.com>
2015-02-03 07:27:31 +0000

    solves execise 3.1 and 3.2

--- liangjiaquan/sample_app/config/routes.rb
+++ liangjiaquan/sample_app/config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  #get 'static_pages/home'
 
   get 'static_pages/help'
   
   get 'static_pages/about'
+  
+  get 'static_pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- liangjiaquan/sample_app/test/controllers/static_pages_controller_test.rb
+++ liangjiaquan/sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,34 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # call the function before every test
+  # solves execise 3.1
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
+  end
+  
+  # solves execise 3.2
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
