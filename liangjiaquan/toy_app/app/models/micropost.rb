b2605aa16b04f80582b2c024d7648f79341e7ae9
AssIstne <463636701@qq.com>
2015-02-03 03:28:09 +0000

    practice of chapter 2

--- liangjiaquan/toy_app/app/models/micropost.rb
+++ liangjiaquan/toy_app/app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  validates :content, length: { maximum: 14 }
+  validates :content, length: { maximum: 14 }, 
+  # practice1 of chapter2
+  presence: true
 end
--- liangjiaquan/toy_app/app/models/user.rb
+++ liangjiaquan/toy_app/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_many :microposts
+  # practice2 of chapter2
+  validates :name, presence: true
+  validates :email, presence: true
 end
