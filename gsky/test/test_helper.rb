342a515fe3d4dcf757714ef32306eb750b81e8bb
gsky <gsky526@163.com>
2015-02-05 20:03:15 +0800

    finish sessions

--- gsky/test/test_helper.rb
+++ gsky/test/test_helper.rb
@@ -12,4 +12,25 @@ class ActiveSupport::TestCase
   end
 
   # Add more helper methods to be used by all tests here...
+
+#登入测试用户
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+	if integration_test?
+          post login_path, session: { email:       user.email,
+                                      password:    password,
+                                      remember_me: remember_me }
+	else
+		session[:user_id] = user.id 
+	end
+	
+end
+	
+	private
+		#在集成测试中返回true
+		def integration_test?
+			defined?(post_via_redirect)
+		end
+
 end
