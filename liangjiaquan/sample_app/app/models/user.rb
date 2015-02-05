4e1efba98fd778491b20e54cde654cb8e73c1fed
AssIstne <463636701@qq.com>
2015-02-05 01:53:47 +0000

    add password to user-model

--- liangjiaquan/sample_app/app/models/user.rb
+++ liangjiaquan/sample_app/app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  # downcase email before save
+  before_save { self.email = email.downcase }
+  # valid name
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w\+\-.]+@[\w\-.]+.[a-z]+\z/i
+  # valid email
+  VALID_EMAIL_REGEX = /\A[\w\+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255}, 
   format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
+  has_secure_password
+  
+  validates :password, length: { minimum: 6 }
 end
