5f2ebbf5ddec35cc994a71afc5bce4685f36b67d
AssIstne <463636701@qq.com>
2015-02-05 15:15:44 +0000

    finish signup page and its test

--- liangjiaquan/sample_app/test/integration/users_signup_test.rb
+++ liangjiaquan/sample_app/test/integration/users_signup_test.rb
@@ -12,4 +12,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       end
       assert_template 'users/new'
   end
+  
+  test "valid signup information" do
+    get signup_path
+    name = "Example User"
+    email = "user@example.com"
+    password = "password"
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, 
+      user: { name: name, email: email, password: password, password_confirmation: password }
+    end
+    assert_template 'users/show'
+  end
 end
