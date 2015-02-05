da55cc433c227b735be5c8903634201834a168bd
AssIstne <463636701@qq.com>
2015-02-05 02:18:48 +0000

    solves execise 6

--- liangjiaquan/sample_app/test/models/user_test.rb
+++ liangjiaquan/sample_app/test/models/user_test.rb
@@ -40,7 +40,10 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@example,com user_at_foo.org 
+    user.name@example. foo@bar_baz.com foo@bar+baz.com
+    foo@bar...com #execise 6.3
+    ]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -63,6 +66,14 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+  
+  # execise 6.1
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "USER@example.COM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal @user.reload.email, mixed_case_email.downcase
   end
   # test "the truth" do
   #   assert true
