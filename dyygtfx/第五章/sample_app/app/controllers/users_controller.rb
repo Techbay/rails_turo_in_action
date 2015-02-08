786ce6e88abaacf66a4a89992287dbe946ce700f
Go7hic <gtfx0209@qq.com>
2015-02-09 00:40:59 +0800

    第九章

--- dyygtfx/第五章/sample_app/app/controllers/users_controller.rb
+++ dyygtfx/第五章/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,only: [:edit,:update]
+  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -15,10 +17,36 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @user = User.find(parms[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params
+      # 处理更新成功的情况
+      flash[:success] = "profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  # 确保用户已登录
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+
+    redirect_to(root_url) unless current_user?(@user)
+  end
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
             :password_confirmation)
     end
-  end
+end
 
--- dyygtfx/第五章/sample_app/app/helpers/sessions_helper.rb
+++ dyygtfx/第五章/sample_app/app/helpers/sessions_helper.rb
@@ -18,6 +18,9 @@ module SessionsHelper
       end
     end
   end
+  def current_user?(user)
+    user == current_user
+  end
   def logged_in?
     !current_user.nil?
   end
