a1ad2868eff4a499fe9b9ec708ed0b0a5e8f82c2
ynzkai <ynzkai@gmail.com>
2015-02-09 00:10:29 +0800

    Finish log in/log out

--- ynzkai/sample_app/test/integration/users_signup_test.rb
+++ ynzkai/sample_app/test/integration/users_signup_test.rb
@@ -24,5 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password_confirmation: password}
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
