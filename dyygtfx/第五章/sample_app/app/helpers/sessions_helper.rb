43784387dcdb94e726d853f9ab77e5c870ce0630
Go7hic <gtfx0209@qq.com>
2015-02-10 02:08:23 +0800

    第九章

--- dyygtfx/第五章/sample_app/app/helpers/sessions_helper.rb
+++ dyygtfx/第五章/sample_app/app/helpers/sessions_helper.rb
@@ -39,4 +39,13 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  # 重定向到存储的地址，或者默认地址
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  # 存储以后需要获取的地址
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
--- dyygtfx/第五章/sample_app/test/controllers/users_controller_test.rb
+++ dyygtfx/第五章/sample_app/test/controllers/users_controller_test.rb
@@ -27,4 +27,22 @@ class UsersControllerTest < ActionController::TestCase
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
     assert_redirected_to root_url
   end
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+  assert_redirected_to root_url
+  end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
 end
