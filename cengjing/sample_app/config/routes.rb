49dc636b2eec7ec1fb61cbc825fa49c15c8c4d64
cengjing <hnbianch@gmail.com>
2015-02-09 13:37:52 +0800

    Use SSL

--- cengjing/sample_app/config/routes.rb
+++ cengjing/sample_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
 
+  get 'users/show'
+
   #get 'static_pages/home'
 
   get 'help' => 'static_pages#help'
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   
   get 'signup' => 'users#new'
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
