97d3045466b277cd9d1cf798f711693370e01b32
ynzkai <ynzkai@gmail.com>
2015-02-09 23:29:39 +0800

    Finish user edit, update, index, and destroy actions

--- ynzkai/sample_app/app/models/user.rb
+++ ynzkai/sample_app/app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
-  validates :password, length: {minimum: 6}
+  validates :password, length: {minimum: 6}, allow_blank: true
 
   has_secure_password
 
