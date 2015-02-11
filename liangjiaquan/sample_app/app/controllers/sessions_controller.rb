207d4ff8bbd6ea8745d5e31974d2f551040fe2db
AssIstne <463636701@qq.com>
2015-02-11 11:11:12 +0800

    finished chapter 8

--- liangjiaquan/sample_app/app/controllers/sessions_controller.rb
+++ liangjiaquan/sample_app/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			# loign
 			log_in user
-			remember user
+			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 			redirect_to user
 		else
 			# failed to login
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		log_out
+		log_out if logged_in?
 		redirect_to root_url
 	end
 end
--- liangjiaquan/sample_app/app/models/user.rb
+++ liangjiaquan/sample_app/app/models/user.rb
@@ -31,6 +31,12 @@ class User < ActiveRecord::Base
 	end
 
 	def authenticated?(remember_token)
+		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+		
 	end
 end
