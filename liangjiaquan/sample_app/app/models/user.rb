d7ce3a451620a813f63aa40225348cf12769c6f1
AssIstne <463636701@qq.com>
2015-02-12 17:10:11 +0800

    finished chapter 10

--- liangjiaquan/sample_app/app/models/user.rb
+++ liangjiaquan/sample_app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
   # downcase email before save
   # before_save { self.email = email.downcase }
   # execise 6.2
@@ -50,6 +50,21 @@ class User < ActiveRecord::Base
   def send_activation_email
   	UserMailer.account_activation(self).deliver_now
   end
+
+  def create_reset_digest
+	  self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+	  UserMailer.password_reset(self).deliver_now
+  end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
+	end
+
 	private
 	
 	  def downcase_email
