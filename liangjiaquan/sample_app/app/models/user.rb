2e5dd4b3f3ba52818c2ffe2607f2d926e04bf9f2
AssIstne <463636701@qq.com>
2015-02-13 17:36:57 +0800

    chapter 12 follower and followed

--- liangjiaquan/sample_app/app/models/user.rb
+++ liangjiaquan/sample_app/app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship",
+	         foreign_key: "follower_id",
+					 dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+	         foreign_key: "followed_id",
+					 dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 	attr_accessor :remember_token, :activation_token, :reset_token
   # downcase email before save
   # before_save { self.email = email.downcase }
@@ -70,6 +78,17 @@ class User < ActiveRecord::Base
   	Micropost.where("user_id = ?", id)
   end
 
+  def follow(other_user)
+  	active_relationships.create(followed_id: other_user.id)
+  end
+
+	def unfollow(other_user)
+	  active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	def following?(other_user)
+	  following.include?(other_user)
+	end
 	private
 	
 	  def downcase_email
