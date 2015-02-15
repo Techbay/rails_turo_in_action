6295a169b181cdef518c28a4d1f8265402704306
gsky <gsky526@163.com>
2015-02-15 10:43:58 +0800

    update user

--- gsky/app/controllers/users_controller.rb
+++ gsky/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  
+   before_action :logged_in_user, only:[:edit,:update]
+   before_action :correct_user, only:[:edit,:update]
+
   def new
     @user = User.new
   end
@@ -18,8 +22,38 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit  #进入编辑用户界面
+    @user = User.find(params[:id])
+  end
+
+  def update  #提交编辑用户
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+
+    #确保用户已登录
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    #确保是正确的用户
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
