34354107bdf6bce5ec346991da21eae86d9ebad4
ynzkai <ynzkai@gmail.com>
2015-02-21 09:41:34 +0800

    Add user following

--- ynzkai/sample_app/app/controllers/static_pages_controller.rb
+++ ynzkai/sample_app/app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
-    @feed_items = current_user.feed.paginate(page: params[:page]) if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
